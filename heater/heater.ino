3988b620cd815b8a739554ad33382769f26bfc32
cyc-987 <2516803593@qq.com>
2024-04-22 19:38:48 +0800

    add: heater

---
+++ heater/heater.ino
@@ -0,0 +1,19 @@
+#define heater A4//高关低开，继电器是反的（x，别一直加热了
+
+void setup()
+{
+    Serial.begin(115200);
+    pinMode(heater, OUTPUT);
+}
+void loop()
+{
+    digitalWrite(heater, HIGH);
+    Serial.println("write high but close");
+    Serial.println(analogRead(heaterReturn));
+    delay(5000);
+
+    digitalWrite(heater, LOW);
+    Serial.println("write low but open");
+    Serial.println(analogRead(heaterReturn));
+    delay(10000);
+}
