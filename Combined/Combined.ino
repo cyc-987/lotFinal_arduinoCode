87c813a7f9418c081396cf1c4275389884b77a3d
hxm <hxm15726861286@gmail.com>
2024-04-24 22:51:20 +0800

    剩余语音和IIC冲突待解决

--- Combined/Combined.ino
+++ Combined/Combined.ino
@@ -5,17 +5,23 @@
 #include <ArduinoJson.h>
 #include <SPI.h>
 #include <U8g2lib.h>
+#include <Adafruit_MLX90614.h>
+#include <dht11.h>
+// #include <SoftwareWire.h>
 
+//wifi和mqtt参数——————————————————————————————————————————————————————————————————————————————
 // User Modified Part
-#define wifi_ssid ""
-#define wifi_psw ""
-#define clientIDstr ""
-#define timestamp ""
-#define ProductKey ""
-#define DeviceName ""
-#define DeviceSecret ""
-#define password ""
+#define wifi_ssid "xm"
+#define wifi_psw "12090217"
+#define clientIDstr "Smart_takeout_cabinet"
+#define timestamp "666"
+#define ProductKey "k0v6oal29SX"
+#define DeviceName "Smart_takeout_cabinet"
+#define DeviceSecret "74256bc8a9ecd481f60fa63c74226e43"
+#define password "A3BC01B92D5005113580F007FD58FFADB7DE4AEE"
 
+
+//引脚宏定义————————————————————————————————————————————————————————————————————————————————
 // Logic Preset
 #define OFF 0
 #define ON 1
@@ -26,6 +32,22 @@
 #define Buzzer_ON digitalWrite(BuzzerPin, HIGH)
 #define Buzzer_OFF digitalWrite(BuzzerPin, LOW)
 
+//fan
+#define fanPin 11
+//lock
+#define lockInput A4
+#define lockOutput A5
+//heater
+#define heater A2//高关低开，继电器是反的（x，别一直加热了
+//DHT11
+#define DHT11_PIN A0
+
+#define SDA_PIN 20
+#define SCL_PIN 21
+
+
+#define My_JSON_PACK        "{\"id\":\"66666\",\"version\":\"1.0\",\"method\":\"thing.event.property.post\",\"params\":{\"temperature\":%f,\"humidity\":%f,\"hasTakeout\":%d,\"heaterStatus\":%d,\"fanStatus\":%d}}\r"
+//AT指令————————————————————————————————————————————————————————————————————————————
 // ATcmd Format
 #define AT "AT\r"
 #define AT_OK "OK"
@@ -44,7 +66,8 @@
 #define AT_MQTT_ALIVE "AT+MQTTKEEPALIVE=500\r"
 #define AT_MQTT_START "AT+MQTTSTART\r"
 #define AT_MQTT_START_SUCC "+MQTTEVENT:CONNECT,SUCCESS"
-#define AT_MQTT_PUB_SET "AT+MQTTPUB=/%s/%s/user/Arduino,1\r"
+// #define AT_MQTT_PUB_SET "AT+MQTTPUB=/%s/%s/user/Arduino,1\r"
+#define AT_MQTT_PUB_SET       "AT+MQTTPUB=/sys/%s/%s/thing/event/property/post,1\r"
 #define AT_MQTT_PUB_ALARM_SET "AT+MQTTPUB=/sys/%s/%s/thing/event/GasAlarm/post,1\r"
 #define AT_MQTT_PUB_DATA "AT+MQTTSEND=%d\r"
 #define JSON_DATA_PACK "{\"id\":\"100\",\"version\":\"1.0\",\"method\":\"thing.event.property.post\",\"params\":{\"RoomTemp\":%d.%02d,\"AC\":%d,\"Fan\":%d,\"Buzzer\":%d,\"GasDetector\":%d}}\r"
@@ -67,6 +90,8 @@ char ATdata[BUF_LEN_DATA];
 #define BuzzerPin 3
 int Buzzer = OFF;
 
+
+//变量定义————————————————————————————————————————————————————————————————————————————————
 StaticJsonDocument<300> doc;
 // mydata
 String data; // new add
@@ -81,23 +106,16 @@ float targetHumidity;
 int voiceOutput;//0 is no output,,
 unsigned long timeStart;
 
-// my start data
-//fan
-#define fanPin 11
+
+
 //dht11
-#include <dht11.h>
 dht11 DHT;
-#define DHT11_PIN A0
-//oled
+// //oled
 U8G2_SSD1306_128X64_NONAME_1_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
-//lock
-#define lockInput A4
-#define lockOutput A5
-//heater
-#define heater A2//高关低开，继电器是反的（x，别一直加热了
+
 //infrared temp
-#include <Adafruit_MLX90614.h>
-Adafruit_MLX90614 mlx = Adafruit_MLX90614();
+Adafruit_MLX90614 mlx = Adafruit_MLX90614(); 
+
 
 void setup()
 {
@@ -109,7 +127,7 @@ void setup()
   String inString = "";
 
   //  Pin_init();
-  Serial.println("Start");
+  Serial.println("Setup Start");
 
   // Cloud Initial
   while (1)
@@ -129,7 +147,12 @@ void setup()
   lockInit();
   heaterInit();
   itemperatureInit();
+  Serial.println("dataInit Done");
 }
+
+
+
+
 
 void loop()
 {
@@ -141,25 +164,50 @@ void loop()
   {
     delay(10);
     inString = Serial3.readString();
-    if (inString != "")
+
+    if (inString.indexOf("MQTTRECV") != -1)
     {
+      Serial.println("==============print aliyun receive data================");
+      Serial.print("json:");
+      Serial.println(inString);
       data = inString;
       parse(data);
+      Serial.println("==================End====================\n\n\n");
     }
-    // Serial.println(data);
+  
   }
+  
 
   if (Serial3.available() == 0)
   {
     updateData();
   }
 
+
+//五秒上传一次数据
   if ((millis() - timeStart) > 5000)
-  {
+  { 
     getData();
+    Serial.println("==================print Data=================");
+    Serial.println("temperature: "+String(temperature));
+    Serial.println("humidity: "+String(humidity));
+    Serial.println("lockStatus: "+String(lockStatus));
+    Serial.println("hasTakeout: "+String(hasTakeout));
+    Serial.println("heaterStatus: "+String(heaterStatus));
+    Serial.println("fanStatus: "+String(fanStatus));
+    Serial.println("targetTemperature: "+String(targetTemperature));
+    Serial.println("targetHumidity: "+String(targetHumidity));
+    Serial.println("voiceOutput: "+String(voiceOutput));
+    Serial.println("==================End====================\n\n\n");
+
+    
+    
     Upload();
+    // oledDisplay_th();
     timeStart = millis();
   }
+
+
 }
 
 // 设置了一个解析的函数，利用库ArdunioJson版本是6.2.13
@@ -188,18 +236,33 @@ int parse(String data)
   // 获取数据
   const char *method = doc["method"];
   const char *id = doc["id"];
-  lockStatus = doc["params"]["lockStatus"];
-  targetTemperature = doc["params"]["targetTemperature"];
-  targetHumidity = doc["params"]["targetHumidity"];
-  voiceOutput = doc["params"]["voiceOutput"];
+if (doc.containsKey("params") && doc["params"].containsKey("lockStatus")) {
+    lockStatus = doc["params"]["lockStatus"];
+}
+
+if (doc.containsKey("params") && doc["params"].containsKey("targetTemperature")) {
+    targetTemperature = doc["params"]["targetTemperature"];
+}
+
+if (doc.containsKey("params") && doc["params"].containsKey("targetHumidity")) {
+    targetHumidity = doc["params"]["targetHumidity"];
+}
+
+if (doc.containsKey("params") && doc["params"].containsKey("voiceOutput")) {
+    voiceOutput = doc["params"]["voiceOutput"];
+}
 
   // Print values.
-  Serial.println("==============Start================");
+  // Serial.println("==============Start================");
   // Serial.println(method);
-  // Serial.println(id);
+  // Serial.println(id)
+  Serial.print("lockStatus: ");
   Serial.println(lockStatus);
+  Serial.print("targetTemperature: ");
   Serial.println(targetTemperature);
+  Serial.print("targetHumidity: ");
   Serial.println(targetHumidity);
+  Serial.print("voiceOutput: ");
   Serial.println(voiceOutput);
   return 0;
 }
@@ -208,7 +271,7 @@ bool Upload()
 {
   bool flag;
   int len;
-
+  Serial.println("===============Upload==============");
   //AT connect
   cleanBuffer(ATcmd, BUF_LEN);
   snprintf(ATcmd, BUF_LEN, AT_MQTT_PUB_SET, ProductKey, DeviceName);
@@ -216,7 +279,7 @@ bool Upload()
 
   //prepare data to upload
   cleanBuffer(ATdata, BUF_LEN_DATA);
-  len = snprintf(ATdata, BUF_LEN_DATA, JSON_DATA_PACK_3, ProductKey, DeviceName,temperature,humidity);
+  len = snprintf(ATdata, BUF_LEN_DATA, My_JSON_PACK, temperature,humidity,hasTakeout,heaterStatus,fanStatus);
   Serial.println(ATdata);
 
   //upload data
@@ -226,8 +289,45 @@ bool Upload()
   if (flag)
     flag = check_send_cmd(ATdata, AT_MQTT_PUB_DATA_SUCC, 20);
 
+  Serial.println("==================Upload End=================\n\n\n");
   return flag;
 }
+
+
+// bool Upload()
+// {
+//   bool flag;
+//   int  frac;
+//   int zs;
+//   int len;
+//   cleanBuffer(ATcmd,BUF_LEN);
+//   snprintf(ATcmd,BUF_LEN,AT_MQTT_PUB_SET,ProductKey,DeviceName);
+//   flag = check_send_cmd(ATcmd,AT_OK,DEFAULT_TIMEOUT);
+//   if(!flag) {
+//     Serial.println("send AT_MQTT_PUB_SET false");
+//     return false;
+//   }
+
+//   cleanBuffer(ATdata,BUF_LEN_DATA);
+//   zs=(int) temperature;
+//   frac=(temperature-zs)*100;
+//   len = snprintf(ATdata,BUF_LEN_DATA,My_JSON_PACK,temperature,humidity,hasTakeout,heaterStatus,fanStatus);
+
+//   cleanBuffer(ATcmd,BUF_LEN);
+//   snprintf(ATcmd,BUF_LEN,AT_MQTT_PUB_DATA,len-1);
+//   flag=0;
+//   flag = check_send_cmd(ATcmd,">",DEFAULT_TIMEOUT);
+//   if(flag) {
+//     flag = check_send_cmd(ATdata,AT_MQTT_PUB_DATA_SUCC,20);
+    
+//   } 
+//   else {
+//     Serial.println("> accept wrong");
+//     return false;
+//   }
+//     return true;
+
+// }
 
 bool Ali_connect()
 {
--- Combined/readme.md
+++ Combined/readme.md
@@ -4,3 +4,6 @@ ArduinoIDE中编译通过
 Sketch uses 25322 bytes (9%) of program storage space. Maximum is 253952 bytes.
 Global variables use 3353 bytes (40%) of dynamic memory, leaving 4839 bytes for local variables. Maximum is 8192 bytes.
 ```
+编译输出信息为：
+Sketch uses 28190 bytes (11%) of program storage space. Maximum is 253952 bytes.
+Global variables use 3867 bytes (47%) of dynamic memory, leaving 4325 bytes for local variables. Maximum is 8192 bytes.
